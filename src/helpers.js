e481c38f17c5a40df6b28a0985e35500c92b9fbd
zaba <zaba@ksp.sk>
2018-11-26 16:45:24 +0100

    pridane helpre pre aury

--- src/helpers.js
+++ src/helpers.js
@@ -9,7 +9,7 @@ By convention, the function receives three params
 WHAT TO HANDLE:
 - creature cannot go bellow 0
 */
-
+import { creatureSpells, fighterSpells } from './spells'
 
 // Damage helpers
 export const powerDmg = (creature, amount, state) => {
@@ -40,7 +40,39 @@ export const levelAndManaCostEnabled = (fighter, spellID, manaCost) => {
   return true
 }
 
+// Crate fighters helpers
 
-// Auras helpers
-export const applyAuras = ({state, applier}) => {
+export const addFighter = (state, newFighter) => {
+	// apply auras from others to new fighter
+	for (const fighter of state.fighters) {
+		for (const spell of fighterSpells[fighter.race]) {
+			if (spell.passive && spell.isEnabled && spell.isEnabled({fighter}) && spell.doesApply && spell.DoesApply(newFighter, fighter, state)) {
+				spell.applyAura(newFighter, fighter, state)
+			}
+		}
+	}
+	// apply auras from monsters to new fighter
+	for (const monster of state.creatures) {
+		for (let i = 0; i < monster.spellIndexes.length; i++) {
+			if (monster.spellIndexes[i] && creatureSpells[i].doesApply && creatureSpells[i].doesApply(newFighter, monster, state)) {
+				creatureSpells[i].applyAura(newFighter, monster, state)
+			}
+		}
+	}
+	// apply auras from new fighter to others
+	for (const spell of fighterSpells[newFighter.race]) {
+		for (const fighter of state.fighters) {
+			if (spell.passive && spell.isEnabled && spell.isEnabled({newFighter}) && spell.doesApply && spell.DoesApply(fighter, newFighter, state)) {
+				spell.applyAura(fighter, newFighter, state)
+			}
+		}
+	}
+	// apply auras from new fighter to monsters
+	for (const spell of fighterSpells[newFighter.race]) {
+		for (const monster of state.creatures) {
+			if (spell.passive && spell.isEnabled && spell.isEnabled({newFighter}) && spell.doesApply && spell.DoesApply(monster, newFighter, state)) {
+				spell.applyAura(monster, newFighter, state)
+			}
+		}
+	}
 }
